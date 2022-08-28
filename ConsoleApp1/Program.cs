ccd9d2452d087b6439f8ad3cfb5673cdd85acbf7
StarlinkG <donikovv228@yandex.ru>
2022-08-28 16:10:46 +0300

    added method for Update contract&Rename wallet

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        var currency = new Currency { WalletName = "Dollars", WalletValue = 687 };
+        ChangeWallet(ref currency, "Euro", 666);
+        Console.WriteLine($"{currency.WalletValue}{currency.WalletName}");
+
+
+        var employee1 = new Employee { FirstName = "Elon", LastName = "Musk" };
+
+        employee1.Contract = UpdateContract(employee1.FirstName, employee1.LastName, employee1.Contract);
+        Console.WriteLine(employee1.Contract);
+
+    }
+    static string UpdateContract(string FirstName, string LastName, string Contract)
+
+    {
+
+        return Contract = ($"{FirstName} {LastName} was invited for 2 years");
+
+    }
+
+    public static void ChangeWallet(ref Currency currency, string NewName, int NewValue)
+    {
+        currency.WalletName = NewName;
+        currency.WalletValue = NewValue;
+        NewName = "Euro";
+        NewValue = 666;
+
+
+
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Models
+{
+    public class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    public class Employee : Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Contract { get; set; }
+    }
+
+    public class Clients
+    {
+
+    }
+
+    public struct Currency
+    {
+        public string WalletName { get; set; }
+        public int WalletValue { get; set; }
+
+    }
+
+}
